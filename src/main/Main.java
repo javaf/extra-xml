fd81f8fb427e5b7f7335d88bcc8c19a27e843dcc
Subhajit Sahu <wolfram77@gmail.com>
2015-02-25 15:33:55 +0530

    lastChild tested

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,6 +11,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Xml data = new Xml("<data><user><id>wolfram77</id><pass>incorrect</pass></user></data>");
         Xml user = data.child();
-        System.out.println(user.child("abc"));
+        System.out.println(user.lastChild("pass"));
     }
 }
