753c5fdb4a599a66c65967a893fb422772189c6e
Subhajit Sahu <wolfram77@gmail.com>
2015-02-25 15:35:01 +0530

    children tested

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,6 +11,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Xml data = new Xml("<data><user><id>wolfram77</id><pass>incorrect</pass></user></data>");
         Xml user = data.child();
-        System.out.println(user.lastChild("pass"));
+        for(Xml child : user.children("id"))
+            System.out.println(child.name());
     }
 }
