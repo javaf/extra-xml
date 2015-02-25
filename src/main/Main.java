d46ab3f56f52f211d4e52723b206af63607dbb31
Subhajit Sahu <wolfram77@gmail.com>
2015-02-25 15:37:19 +0530

    elem, copy tested

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,6 +11,8 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Xml data = new Xml("<data><user><id>wolfram77</id><pass>incorrect</pass></user></data>");
         Xml user = data.child();
+        user.add(user.elem("id"));
+        user.add(user.child("id").copy());
         for(Xml child : user.children("id"))
             System.out.println(child.name());
     }
