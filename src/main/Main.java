222f84d3f8b67c6952c4835027714e5336638644
Subhajit Sahu <wolfram77@gmail.com>
2015-02-25 15:09:16 +0530

    toString test done

---
+++ src/main/Main.java
@@ -0,0 +1,15 @@
+// @wolfram77
+package main;
+
+// required modules
+import org.data.*;
+
+
+
+public class Main {
+    
+    public static void main(String[] args) throws Exception {
+        Xml data = new Xml("<data><user><id>wolfram77</id><pass>incorrect</pass></user></data>");
+        System.out.println(data);
+    }
+}
