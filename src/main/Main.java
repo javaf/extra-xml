2f84eb5643f851b7420d5c452aa71a8119c93da4
Subhajit Sahu <wolfram77@gmail.com>
2015-02-25 15:41:26 +0530

    name, val tested

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,8 +11,8 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Xml data = new Xml("<data><user><id>wolfram77</id><pass>incorrect</pass></user></data>");
         Xml user = data.child();
-        user.child("id").remove();
+        user.child("id").name("ids").val("oye");
         for(Xml child : user.children())
-            System.out.println(child.name());
+            System.out.println(child.name()+" : "+child.val());
     }
 }
